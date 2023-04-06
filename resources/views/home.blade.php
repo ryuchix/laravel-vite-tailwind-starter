463336c075f511c470d8d1f22590bba9b036aab3
Randy <evilryok@gmail.com>
2023-04-06 10:36:59 +0800

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+your content here.
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>@yield('title') | {{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+        <!-- Scripts and css -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @yield('css')
+    </head>
+
+    <body class="">
+        <div class="flex p-5">
+            <main class="w-full ml-0 md:ml-80 md:pl-5">
+                @yield('content')
+            </main>
+        </div>
+
+        <!-- javascript -->
+        @yield('js')
+        @stack('script')
+    </body>
+</html>
